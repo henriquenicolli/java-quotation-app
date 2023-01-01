db47ec7ce49f344dde62930f72ed7cc9692ec93f
Henrique Biondo Nicolli Soares <henriquenicolli@Henriques-MacBook-Air.local>
2023-01-01 19:02:53 -0300

    add project

---
+++ src/main/java/com/quotationapp/QuotationAppApplication.java
@@ -0,0 +1,13 @@
+package com.quotationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quotationapp/QuotationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quotationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
